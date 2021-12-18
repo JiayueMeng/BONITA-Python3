99ecc16f0ece9eab0d3b0908cfcd15db3e6a901c
Jiayue Meng <90874903+JiayueMeng@users.noreply.github.com>
2021-12-18 13:39:23 -0500

    updated to use conda environment

--- calcNodeImportancesubmit.sh
+++ calcNodeImportancesubmit.sh
@@ -7,6 +7,7 @@
 #SBATCH -n 1
 #SBATCH -c 1
 
-module load intelpython/2.7.12
+module load anaconda3/2020.11
+activate scBONITA
 python pathway_analysis_score_nodes.py $1 $SLURM_ARRAY_TASK_ID
 echo "ran GA"
